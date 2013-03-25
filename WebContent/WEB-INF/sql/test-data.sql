45c0c06c2c713428707a080c5883413d455bbba2
de20ce <javinwd@yahoo.fr>
2013-03-25 15:52:43 +0100

    some corrections

---
+++ WebContent/WEB-INF/sql/test-data.sql
@@ -0,0 +1,2 @@
+insert into vgn_mem values('AABBAABB','DOB1','EMAIL1','phone1','Address',78190,'France');
+insert into vgn_mem values('BGDBCBDB','DOB2','EMAIL2','phone2','Address2',78360,'France');
---
+++ src/com/controller/JBTJdbcController.java
@@ -0,0 +1,52 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.beans.VngMem;
+@Controller
+public class JBTJdbcController {
+	
+	@Autowired
+	com.service.SpringJdbcService mfssService;
+
+	@RequestMapping(value = "/JdbcInsert", method = RequestMethod.GET)
+	public ModelAndView JdbcInsert() {
+		System.out.println("Controller__");
+		//ModelAndView mav = new ModelAndView("JdbcInsert");
+		return new ModelAndView("JdbcInsert","command", new VngMem());
+
+	}
+	
+	@RequestMapping(value = "/insertJdbcContact", method = RequestMethod.POST)
+	public String insertContact(@ModelAttribute("SpringWeb") VngMem vngmem, ModelMap model) {
+		System.out.println("Controller "+ vngmem.getCountry());
+		model.addAttribute("name",vngmem.getName());
+		model.addAttribute("dob",vngmem.getDob());
+		model.addAttribute("email",vngmem.getEmail());
+		model.addAttribute("phone",vngmem.getPhone());
+		model.addAttribute("address",vngmem.getAddress());
+		model.addAttribute("pincode",vngmem.getPincode());
+		model.addAttribute("country",vngmem.getCountry());
+		try {
+			
+			//mfssService.insertMemDts(vngmem);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Controller end");
+		return "insertResult";
+		
+	}
+
+	
+
+}
